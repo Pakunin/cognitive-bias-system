3d91ac1f525ab02aa496ccf3ca5f6aeb5ced1b0e
Aniruddha Dutta <dutta.aniruddha2006@gmail.com>
2026-04-08 03:05:35 +0530

    updated config for apis

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_PATH = os.getenv("DB_PATH", "data/emotions.sqlite")
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY", "")
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID", "")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET", "")
